a7553590fead16773da1ff055224e10628db3f95
shshshshshan <21-1-01040@vsu.edu.ph>
2025-06-28 13:27:01 +0800

    [Frontend] Refactor Google Image. Add cursor-pointer to ui/button and fix minor error in auth-button

--- src/components/auth-button.tsx
+++ src/components/auth-button.tsx
@@ -20,8 +20,8 @@ export async function AuthButton() {
       <Button asChild size="sm" variant="outline">
         <Link href="/auth/login">Sign in</Link>
       </Button>
-      <Button asChild size="sm" variant={"default"}>
-        <Link href="/auth/sign-up">Sign up</Link>More actions
+      <Button size="sm" variant="default">
+        <Link href="/auth/sign-up">Sign up</Link>
       </Button>
     </div>
   );
